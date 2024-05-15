2a5a31b8dfdc7df023c87fd237907b5512eeb7a2
Mika Lagendijk <90611507+SoaringSailor@users.noreply.github.com>
2024-05-15 13:34:58 +0200

    Add sensible base config

--- lua/soaringsailor/keymaps.lua
+++ lua/soaringsailor/keymaps.lua
@@ -1,6 +1,43 @@
 local map = vim.keymap.set
 local command = vim.api.nvim_create_user_command
 
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+-- buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
 -- command and mapping to change directory to dir of current buffer
 command("CDC", "cd %:p:h", {})
 map("n", "<leader>bc", "<cmd>CDC<cr>", { noremap = true, silent = false, desc = "Switch to Buffer Directory" })
--- lua/soaringsailor/options.lua
+++ lua/soaringsailor/options.lua
@@ -1,5 +1,56 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.wo.number = true -- enable line numbers
+vim.wo.relativenumber = true -- enable relative line numbers
+
 local opt = vim.opt
 
+if not vim.env.SSH_TTY then
+  -- only set clipboard if not in ssh, to make sure the OSC 52
+  -- integration works automatically. Requires Neovim >= 0.10.0
+  opt.clipboard = "unnamedplus" -- Sync with system clipboard
+end
+
+if not vim.g.vscode then
+  opt.timeoutlen = 300 -- lower timeout for quick retrigger when not in vs code
+end
+
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.smoothscroll = true -- enable smooth scrolling for later vim versions
+end
+
+opt.autowrite = true -- automatically write file on :next etc.
+opt.completeopt = "menu,menuone,preview,noselect"
+opt.confirm = true -- ask for confirmation upon exiting with modified content
+opt.cursorline = true -- highlight line of cursor
+opt.expandtab = true -- use spaces instead of tabs
+opt.formatoptions = "jcqrlnt" -- formatting options
+opt.smartcase = true -- ignore case when everything is typed lowercase
+opt.smartindent = true -- automatically add indents at fitting times
+opt.laststatus = 2 -- only last window status bar
+opt.list = true -- show some invisible characters
+opt.pumblend = 10 -- pop up menu transparency
+opt.pumheight = 10 -- pop up menu max entries
+opt.scrolloff = 5 -- Lines of context at top/bottom
+opt.sidescrolloff = 8 -- number of lines when side scrolling for nowrap
+opt.sessionoptions = { "buffers", "curdir", "folds", "tabpages", "terminal", "winsize", "skiprtp", "options", "localoptions" } -- what is restored when saving a session
+opt.shiftwidth = 2 -- number of spaces for indent
+opt.tabstop = 2 -- tab number of spaces
+opt.shiftround = true -- round to shiftwidth size when indenting
+opt.shortmess:append({ W = true, I = true, c = true, C = true }) -- determine which messages to not show
+opt.showmode = false -- we see the mode on the status line
+opt.signcolumn =  "yes" -- always show debugging bar to not shift text
+opt.spelllang = { "en", "de", "nl" } -- spelling languages
+opt.splitkeep = "screen" -- scroll bevaviour when resizing splits
+opt.termguicolors = true -- enable true color support
+opt.undofile = true -- save buffer undos on file write
+opt.undolevels = 10000 -- increase kept changes
+opt.updatetime = 500 -- lower time needed to save to swap and trigger CursorHold
+opt.virtualedit = "block" -- allow non-charachters to be selected in visual select block mode
+opt.wildmode = "longest:full,full" -- Completion mode for command line
+opt.winminwidth = 5 -- minimum size of a window
+opt.foldlevel = 99 -- don't automatically close folds
 opt.conceallevel = 0 -- disable conceal i.e. markdown bold
 opt.mouse = "" -- disable mouse support
 opt.mousehide = true -- hide mouse if possible
@@ -7,4 +58,4 @@ opt.wrap = true -- wrap text if longer than window
 opt.linebreak = true -- break lines at better poitions
 opt.breakindent = true -- indent wrapped lines
 opt.splitright = true -- Always vsplit to right
-opt.splitbelow = true -- always hsplit belo
+opt.splitbelow = true -- always hsplit below
